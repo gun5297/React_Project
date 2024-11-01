ab11f284b66d24f0fe1dfd574355b03da0cf40ab
고건영 <gun5297@naver.com>
2024-11-01 12:39:19 +0900

    11-01 시작

--- project/src/components/Studio/CommentList.jsx
+++ project/src/components/Studio/CommentList.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../../ui/Button';
 import CommentItem from './CommentItem';
 import { CommentListWrap } from './styled';
+import { useSelector } from 'react-redux';
 
 const CommentList = ({ comment, thisChannel }) => {
+    const navigate = useNavigate();
+    const { isLoginUser } = useSelector((state) => state.auth);
     return (
         <CommentListWrap>
             <h3 className='title-h3'>최신 댓글 ({comment.length})</h3>
@@ -15,7 +19,9 @@ const CommentList = ({ comment, thisChannel }) => {
                     />
                 ))}
             </ul>
-            <Button>더 보기</Button>
+            <Button onClick={() => navigate(`/studio/${isLoginUser.user_id}/comment`)}>
+                더 보기
+            </Button>
         </CommentListWrap>
     );
 };
