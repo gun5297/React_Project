8e94cd77c4b5abf10709436d95a7fb098e8958a5
고건영 <gun5297@naver.com>
2024-11-11 12:08:53 +0900

    로컬스토리지

--- project/src/store/index.js
+++ project/src/store/index.js
@@ -2,8 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import auth from './modules/authSlice';
 import channel from './modules/channelSlice';
 import header from './modules/headerSlice';
-import viewingRecord from './modules/viewingRecordSlice';
 
 export const store = configureStore({
-    reducer: { auth, channel, header, viewingRecord },
+    reducer: { auth, channel, header },
 });
--- project/src/store/modules/channelSlice.js
+++ project/src/store/modules/channelSlice.js
@@ -2,18 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Channel } from '../../assets/api/Channel';
 
 const initialState = {
-    Channel,
-    allMovies: [],
+    Channel: JSON.parse(localStorage.getItem('YoutubeChannel')) || Channel,
+    allMovies: JSON.parse(localStorage.getItem('YoutubeAllMovies')) || [],
 };
 
 export const channelSlice = createSlice({
     name: 'channelSlice',
     initialState,
     reducers: {
-        // 10-31 김신영 작업
-        // 채널 정보 수정
-        ChangeChannelInfo(state, action) {},
-        //
+        ChangeChannelInfo(state, action) {
+            // 채널 정보 수정 로직
+            // 수정 후 로컬스토리지 업데이트
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
+        },
+
         AddNewChannel(state, action) {
             const { user_id, user_name, user_email } = action.payload;
             const emailPrefix = user_email.split('@')[0];
@@ -28,22 +30,27 @@ export const channelSlice = createSlice({
                 Movies: [],
             };
             state.Channel[emailPrefix] = NewChannel;
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
         },
-        // 영상 추가
+
         AddNewMovies(state, action) {
-            const NewMove = { movie_id: Math.floor(Math.random() * 999999), ...action.payload };
-            state.Channel[action.payload.movie_channel].Movies.push(NewMove);
+            const NewMovie = { movie_id: Math.floor(Math.random() * 999999), ...action.payload };
+            state.Channel[action.payload.movie_channel].Movies.push(NewMovie);
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
         },
-        // 영상 삭제
+
         DelMovies(state, action) {
             const { channel_name, movie_id } = action.payload;
             const channel = state.Channel[channel_name];
             channel.Movies = channel.Movies.filter((movie) => !movie_id.includes(movie.movie_id));
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
         },
-        // 영상 수정
-        ChangeMovies(state, action) {},
-        // 댓글 추가
-        // 11-08 고건영 작업
+
+        ChangeMovies(state, action) {
+            // 영상 수정 로직
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
+        },
+
         AddNewMoviesComment(state, action) {
             const { movie_id, movie_channel, comment_body, comment_user_id, comment_user_name } =
                 action.payload;
@@ -59,17 +66,25 @@ export const channelSlice = createSlice({
                 (movie) => movie.movie_id === movie_id
             );
             thisMovie.movie_comments.push(newComment);
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
         },
-        // 댓글 삭제
-        DelMoviesComment(state, action) {},
-        // 영상 좋아요 수 변경
-        IsMovieChangeLike(state, action) {},
-        // 전체 동영상 출력 시 실행하는 함수
+
+        DelMoviesComment(state, action) {
+            // 댓글 삭제 로직
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
+        },
+
+        IsMovieChangeLike(state, action) {
+            // 좋아요 수 변경 로직
+            localStorage.setItem('YoutubeChannel', JSON.stringify(state.Channel));
+        },
+
         getAllMovies(state, action) {
             state.allMovies = [];
             Object.keys(state.Channel).forEach((channel) => {
                 state.allMovies.push(...state.Channel[channel].Movies);
             });
+            localStorage.setItem('YoutubeAllMovies', JSON.stringify(state.allMovies));
         },
     },
 });
@@ -84,6 +99,5 @@ export const {
     DelMoviesComment,
     IsMovieChangeLike,
     getAllMovies,
-    SearchMovies,
 } = channelSlice.actions;
 export default channelSlice.reducer;
