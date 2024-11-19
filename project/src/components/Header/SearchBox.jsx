c6a1b3fa658c3896359aa0292071a3b1e8835d8f
고건영 <gun5297@naver.com>
2024-11-19 12:44:23 +0900

    빌드

--- project/src/components/Header/SearchBox.jsx
+++ project/src/components/Header/SearchBox.jsx
@@ -6,6 +6,9 @@ const SearchBox = ({ setIsShown, isShown, search, setSearch }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth.isLoginUser);
+    const { allMovies } = useSelector((state) => state.channel);
+    const { isLoginUser } = useSelector((state) => state.auth);
+    const allTitle = allMovies.map((movie) => movie.movie_title);
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -15,6 +18,7 @@ const SearchBox = ({ setIsShown, isShown, search, setSearch }) => {
             setSearch('');
         } // 검색어 저장
     };
+
     return (
         <form onSubmit={onSubmit} className='search-box' onClick={() => setIsShown(!isShown)}>
             <input
@@ -26,7 +30,19 @@ const SearchBox = ({ setIsShown, isShown, search, setSearch }) => {
                 onChange={(e) => {
                     setSearch(e.target.value);
                 }}
+                list='search-options'
             />
+            <datalist id='search-options'>
+                {isLoginUser.user_search_list.length === 0 &&
+                    allTitle
+                        .filter((title) => title.includes(search))
+                        .slice(0, 10)
+                        .map((item, idx) => (
+                            <option key={idx} value={item}>
+                                {item}
+                            </option>
+                        ))}
+            </datalist>
             <button type='submit' className='search-btn'>
                 <img
                     src='https://raw.githubusercontent.com/React-Project-Team1/data-center/50bcadb338add2ff5dcb6ac28c339d051e58635b/Icon/Search.svg'
